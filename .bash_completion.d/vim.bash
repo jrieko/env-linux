f501ef5e22c0e1c47c5ec7eebb4e51c976c32720
Jenny Taylor <tyjenn@amazon.com>
2019-10-24 17:28:27 -0400

    convert script shebang to use `env`

--- .bash_completion.d/vim.bash
+++ .bash_completion.d/vim.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 debug "file: ${BASH_SOURCE[0]}"
 _vimBash() {
   local cur=${COMP_WORDS[COMP_CWORD]}
--- bin/.vim.bash
+++ bin/.vim.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 error_usage=1
 
